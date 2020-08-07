41a60c04f33149d9fa88356c340068eec86316f1
Andy0414 <pjh8667@naver.com>
2020-08-08 07:27:03 +0900

    Update debug

--- src/socket/page/page.socket.ts
+++ src/socket/page/page.socket.ts
@@ -5,14 +5,14 @@ import Page from "../../schema/Page";
 const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket) => {
 	// pageId, userToken
 	socket.on("joinRoom", async (data) => {
-		let user = await User.quickTokenLogin(data.userToken);
-		if (!user) return;
-		let page = await Page.findById(data.pageId);
-		if (!page) return;
-		if (!(page.ownerPermissionCheck(user) || page.collaborator.indexOf(user._id) != -1)) return;
+		// let user = await User.quickTokenLogin(data.userToken);
+		// if (!user) return;
+		// let page = await Page.findById(data.pageId);
+		// if (!page) return;
+		// if (!(page.ownerPermissionCheck(user) || page.collaborator.indexOf(user._id) != -1)) return;
 		socket.join(data.pageId);
 
-		socket.emit("joinRoom", page);
+		socket.emit("joinRoom", false); // FIXME: DEBUG
 	});
 	// pageId
 	socket.on("update", async (data) => {
